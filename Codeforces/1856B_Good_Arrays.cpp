a971dd1763aed820bfcb0adf99df9fab2de890fe
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-02 23:01:14 +0530

    code completed

--- Codeforces/1856B_Good_Arrays.cpp
+++ Codeforces/1856B_Good_Arrays.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
-#include<vectors>
+#include<vector>
+#include<unordered_set>
 using namespace std;
 
 int main(){
+    int t;cin>>t;
+    bool ans=true;
+    while(t--){
+        int n;cin>>n;
+        
+        if(n==1)ans=false;
 
+        vector<int> v(n);
+
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+
+        unordered_set<int> s;
+
+        for(int x:v){
+            if(s.find(x)==s.end()){
+                s.insert(x);
+            }
+            else{ ans=false;}
+        }
+    }
+    
 }
