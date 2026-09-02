3a6da9e424e79ed7e8f13d600e77c59d924ddd4a
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-02 23:13:54 +0530

    executing

--- Codeforces/1856B_Good_Arrays.cpp
+++ Codeforces/1856B_Good_Arrays.cpp
@@ -6,17 +6,19 @@ int main(){
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
+
         int sum=0;
+        int ones=0;
         vector<int> v(n);
 
         for(int i=0;i<n;i++){
             cin>>v[i];
             sum+=v[i];
+            if(1==v[i])ones++;
         }
 
-        }
-        if(ans==true)cout<<"YES";
-    else cout<<"NO";
+        
+        
     }
     
 }
