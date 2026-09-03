ee122e5a40028fdfce32ba962a987362e3fd275a
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-04 00:26:40 +0530

    2110B B. Down with Brackets

---
+++ Codeforces/2110B_Down with Brackets.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int balance=0;
+        int n;
+        string s;
+        cin>>n>>s;
+        bool check=false;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')balance++;
+            else balance--;
+
+            if(balance==0)check=true;
+        }
+        cout<<(check?"YES\n":"NO\n");
+    }
+}
