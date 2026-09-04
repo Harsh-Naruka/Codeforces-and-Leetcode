8d74c011fee7ef3b091abc9cd3df786bd3d9cd1d
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-04 23:44:14 +0530

    129A_A.Cookies

---
+++ Codeforces/129A_A. Cookies.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    int total=0,odd=0,even=0;
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        total+=v[i];
+        if(v[i]%2==0)even++;
+        else odd++;
+    }
+    if(total%2==0){
+        return even;
+    }
+    else {
+        return odd;
+    }
+}
