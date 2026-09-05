5af120d0172841e7d6c12e5c2479cc5066ab040a
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-05 23:39:28 +0530

    764B

--- Codeforces/746B_B. Decoding.cpp
+++ Codeforces/746B_B. Decoding.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main(){
     int n;cin>>n;
     string s;cin>>s;
-    
+    string a;
+    if(n<3){return s;}
+    else{
+        while(n>2){
+            if(n%2==0){
+                a+=s[(n/2)-1];
+            }
+            else {
+                a+=s[((n+1)/2)-1];
+            }
+            n--;
+        }
+    }
 }
