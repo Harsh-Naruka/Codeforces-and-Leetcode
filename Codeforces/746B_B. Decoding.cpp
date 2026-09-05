f921de99975b5759251060da162b88314187710c
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-05 23:42:39 +0530

    764B

--- Codeforces/746B_B. Decoding.cpp
+++ Codeforces/746B_B. Decoding.cpp
@@ -6,7 +6,7 @@ int main(){
     int n;cin>>n;
     string s;cin>>s;
     string a;
-    if(n<3){return s;}
+    if(n<3){cout<<s;}
     else{
         while(n>2){
             if(n%2==0){
@@ -17,5 +17,10 @@ int main(){
             }
             n--;
         }
+        for(int i=n-3;i<n;i++){
+            a+=s[i];
+        }
+        cout<<a;
     }
+    return 0;
 }
