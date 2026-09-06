682e13426e679bda16f206fb1f40541e6880abe1
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-06 14:53:54 +0530

    709A

---
+++ Codeforces/709A_Juicer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n,b,d;
+    cin>>n>>b>>d;
+    int juice=0;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        if(v[i]<b){
+            juice+=v[i];
+            
+        }
+    }
+
+    cout<<d;
+}
