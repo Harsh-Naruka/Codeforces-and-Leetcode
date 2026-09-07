7c5feda3beb3cece491303427c14c65a6292bc6e
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-08 01:54:28 +0530

    update

--- Codeforces/1988A_Split_the_multiset.cpp
+++ Codeforces/1988A_Split_the_multiset.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
@@ -6,6 +7,6 @@ int main(){
     while(t--){
         int n,k;
         cin>>n>>k;
-        cout<<ceil((n-1)/(k-1))<<'\n';
+        cout<<ceil((double)(n-1)/(k-1))<<'\n';
     }
 }
