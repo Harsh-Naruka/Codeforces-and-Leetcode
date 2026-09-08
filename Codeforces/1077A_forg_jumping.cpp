fa2795de3d02bca297758b19b104fa7721a63044
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-08 14:33:18 +0530

    ..

--- Codeforces/1077A_forg_jumping.cpp
+++ Codeforces/1077A_forg_jumping.cpp
@@ -7,8 +7,8 @@ int main(){
         long long a,b,k;
         cin>>a>>b>>k;
 
-            long long asum=0;
-            long long bsum=0;
+            long long asum=1;
+            long long bsum=1;
             if(k%2==0){
                 asum*=(k/2);
                 bsum*=(k/2);
