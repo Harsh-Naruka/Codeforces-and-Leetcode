a04baab628af6c192fb7beda7f23705690bd8e46
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-10 20:50:57 +0530

    2060C

---
+++ Codeforces/2060C_ Game of Mathletes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        int k;cin>>k;
+        vector<int> v(n);
+        
+        int ans=0;
+        int b=n-1;
+        int a=0;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            
+            if(v[a]+v[b]==k){
+                ans++;
+            }
+            else if(v[a]+v[b]>k){
+                b--;
+            }
+            else{
+                a++;
+            }
+        }
+        cout<<ans<<'\n';
+    }
+}
