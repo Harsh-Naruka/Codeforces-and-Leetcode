b7f250d2f196989cc701b5dc43a126c41202ded8
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-12 06:32:36 +0530

    another change

--- Codeforces/2259C_101.cpp
+++ Codeforces/2259C_101.cpp
@@ -6,7 +6,7 @@ int main(){
     int t;cin>>t;
     while(t--){
         long long n;cin>>n;
-        long long v[n];
+        vector<long long> v(n);
         for(long long i=0;i<n;i++){
             if(v[i]==-1)v[i]=1;
             break;
