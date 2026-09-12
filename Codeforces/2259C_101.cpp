b48e33370049dac837830ca4e2f32ce2805549ec
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-12 06:29:04 +0530

    completed

--- Codeforces/2259C_101.cpp
+++ Codeforces/2259C_101.cpp
@@ -5,13 +5,22 @@ using namespace std;
 int main(){
     int t;cin>>t;
     while(t--){
-        int n;cin>>n;
+        long long n;cin>>n;
         vector<int> v(n);
-        int c1=0;
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-            if(v[i]==-1)c1++;
+        for(long long i=0;i<n;i++){
+            if(v[i]==-1)v[i]=1;
+            break;
         }
-
+        for(long long i=n-1;i>=0;i--){
+            if(v[i]==-1)v[i]=1;
+            break;
+        }
+        for(long long i=0;i<n-1;i++){
+            if(v[i]==-1)v[i]=0;
+        }
+        for(long long i=0;i<n-1;i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
     }
 }
