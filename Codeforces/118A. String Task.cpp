ccc3c557442257e3597da60a48443feb0ba2b77b
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-13 21:28:54 +0530

    118A

--- Codeforces/118A. String Task.cpp
+++ Codeforces/118A. String Task.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 int main(){
     string s;cin>>s;
-    
+    string a;
+    for(int i=0;i<s.size();i++){
+        if(isupper(s[i])){
+            s[i]=tolower(s[i]);
+        }
+    }
+    for(int i=0;i<s.size();i++){
+        if(s[i]!='a' || s[i]!='e' || s[i]!='i'
+        || s[i]!='o' || s[i]!='u' || s[i]!='y'){
+            a[i]+='.';
+            a[i]=s[i];
+        }
+    }
+    for(int i=0;i<a.size();i++){
+        cout<<a[i];
+    }
 }
