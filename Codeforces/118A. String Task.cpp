1b139740a8cbe50916b743279aa6146e08c09b0e
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-13 21:36:55 +0530

    118A

--- Codeforces/118A. String Task.cpp
+++ Codeforces/118A. String Task.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 #include<string>
+#include <cctype>
 using namespace std;
 
 int main(){
     string s;cin>>s;
     string a;
+
     for(int i=0;i<s.size();i++){
-        if(isupper(s[i])){
-            s[i]=tolower(s[i]);
+        s[i]=tolower(s[i]);
+
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i'
+        && s[i]!='o' && s[i]!='u' && s[i]!='y'){
+            a += '.';
+            a += s[i];
         }
     }
-    for(int i=0;i<s.size();i++){
-        if(s[i]!='a' || s[i]!='e' || s[i]!='i'
-        || s[i]!='o' || s[i]!='u' || s[i]!='y'){
-            a[i]+='.';
-            a[i]=s[i];
-        }
-    }
-    for(int i=0;i<a.size();i++){
-        cout<<a[i];
-    }
+    cout<<a;
 }
