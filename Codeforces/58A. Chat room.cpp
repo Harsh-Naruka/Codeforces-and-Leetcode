51d550c169c4bfa254ad5a2ac143cac000aaefe0
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-14 22:13:01 +0530

    58A

--- Codeforces/58A. Chat room.cpp
+++ Codeforces/58A. Chat room.cpp
@@ -5,5 +5,23 @@ using namespace std;
 
 int main(){
     string s;cin>>s;
-    
+    string target = "hello";
+
+    int j = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+
+        if (j == 5) {
+            break;
+        }
+    }
+
+    if (j == 5) {
+        cout << "YE" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 }
