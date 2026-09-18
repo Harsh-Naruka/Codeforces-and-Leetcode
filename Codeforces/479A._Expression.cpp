1a5c03e67e164f47587f1ce4d24dd8e4e91a011a
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-18 23:04:35 +0530

    479A

--- Codeforces/479A._Expression.cpp
+++ Codeforces/479A._Expression.cpp
@@ -6,7 +6,10 @@ int main(){
     int s1=a+(b*c);
     int s2=(a*b)+c;
     int s3=(a*c)+b;
-    if(s1>s2 && s1>s3)return s1;
-    else if(s2>s1 && s2>s3) return s2;
-    else return s3;
+    if(s1>s2 && s1>s3){
+        cout<<s1; 
+    }
+    else if(s2>s1 && s2>s3){cout<<s2;}
+    else{ cout<<s3;}
+    return 0;
 }
