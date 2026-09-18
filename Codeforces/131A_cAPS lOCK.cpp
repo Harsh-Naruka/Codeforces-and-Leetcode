0fc7812f8465c02bafc123febf617e173322c53b
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-18 23:26:44 +0530

    131A

--- Codeforces/131A_cAPS lOCK.cpp
+++ Codeforces/131A_cAPS lOCK.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main(){
     string s;cin>>s;
+    if(islower(s[0])){
     s[0]=toupper(s[0]);
     for(int i=1;i<s.size();i++){
         s[i]=tolower(s[i]);
+    }}
+    else{
+        s[0]=tolower(s[0]);
+    for(int i=1;i<s.size();i++){
+        s[i]=toupper(s[i]);
+    }
     }
     cout<<s;
     return 0;
