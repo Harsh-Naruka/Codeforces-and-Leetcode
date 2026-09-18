92b7d1c92a3659ab68874a1d4699866486b8ccee
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-18 23:37:14 +0530

    131A

--- Codeforces/131A_cAPS lOCK.cpp
+++ Codeforces/131A_cAPS lOCK.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main(){
     string s;cin>>s;
-    if(islower(s[0]) || (isupper(s[0]) && islower(s[1]))){
-    s[0]=toupper(s[0]);
-    for(int i=1;i<s.size();i++){
-        s[i]=tolower(s[i]);
-    }}
-    else{
-        s[0]=tolower(s[0]);
-    for(int i=1;i<s.size();i++){
-        s[i]=toupper(s[i]);
+    int uc=0;
+    for(int i=0;i<s.size();i++){
+        if(isupper(s[i]))uc++;
     }
+
+    if((islower(s[0]) && uc==s.size()-1) || uc==s.size()){
+        s[0]=toupper(s[0]);
+        for(int i=0;i<s.size();i++){
+            s[i]=tolower(s[i]);
+        }
     }
-    cout<<s;
     return 0;
 }
