46943de942208172971458e633cf8ee40b552f60
Harsh-Naruka <hnaruka91@gmail.com>
2026-09-26 00:15:21 +0530

    49A

--- Codeforces/43A_Football.cpp
+++ Codeforces/43A_Football.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main(){
     int n;cin>>n;
-    int a=0;
+   
     map<string,int> mp;
-        string s(n);
+        string s;
         for(int i=0;i<n;i++){
             cin>>s;
             mp[s]++;
         }
         string ans;
+         int a=0;
         for(auto c:mp){
             if(c.second>a){
                 ans=c.first;
